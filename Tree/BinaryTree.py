925fd6db19fbdd437ffd9beff97972b1152158e8
ShapDream <144349058+SharpDream@users.noreply.github.com>
2024-12-22 05:03:21 +0000

    updated binary tree

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -1,61 +1,112 @@
 class BinaryTree:
-  def __init__(self, data):
-      self.data = data
-      self.left = None
-      self.right = None
 
-  def addChild(self, data):
-      if data == self.data:
-          return
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-      if data < self.data:
-          if self.left:
-              self.left.addChild(data)
-          else:
-              self.left = BinaryTree(data)
+    def addChild(self, data):
+        if data == self.data:
+            return
 
-      else:
-          if self.right:
-              self.right.addChild(data)
-          else:
-              self.right = BinaryTree(data)
+        if data < self.data:
+            if self.left:
 
+                self.left.addChild(data)
+            else:
+                self.left = BinaryTree(data)
 
-  def in_order_traversal(self):
-      element = []
+        else:
+            if self.right:
+                self.right.addChild(data)
+            else:
+                self.right = BinaryTree(data)
 
-      # adding left tree if exist
-      if self.left:
-          element += self.left.in_order_traversal()
+    def find_min(self):
+        li = self.in_order_traversal()
+        return li[-1]
 
-      # adding root
-      element.append(self.data)
+    def find_max(self):
+        li = self.in_order_traversal()
+        return li[0]
 
-      # adding right tree if exist
-      if self.right:
-          element += self.right.in_order_traversal()
+    def calculate_sum(self):
+        sum = 0
+        li = self.in_order_traversal()
+        for x in li:
+            sum += x
+        return sum
 
-      return element
+    def search(self, data):
+        if self.data == data:
+            return True
 
-  def search(self, data):
-      if self.data == data:
-          return True
+        if data < self.data:
+            if self.left:
+                return self.left.search(data)
+            else:
+                return False
 
-      if data < self.data:
-          if self.left:
-              return self.left.search(data)
-          else:
-              return False
+        if data > self.data:
+            if self.right:
+                return self.right.search(data)
+            else:
+                return False
 
+    def in_order_traversal(self):
+        # [left, root, right]
+        element = []
 
-      if data > self.data:
-          if self.right:
-              return self.right.search(data)
-          else:
-              return False
+        # adding left tree if exist
+        if self.left:
+            element += self.left.in_order_traversal()
+
+        # adding root
+        element.append(self.data)
+
+        # adding right tree if exist
+        if self.right:
+            element += self.right.in_order_traversal()
+
+        return element
+
+    def post_order_traversal(self):
+        # [root, left, right}
+        element = []
+
+        # adding root
+        element.append(self.data)
+
+        # adding left tree if exist
+        if self.left:
+            element += self.left.in_order_traversal()
+
+        # adding right tree if exist
+        if self.right:
+            element += self.right.in_order_traversal()
+
+        return element
+
+    def pre_order_traversal(self):
+        # [root, left, right}
+        element = []
+
+        # adding left tree if exist
+        if self.left:
+            element += self.left.in_order_traversal()
+
+        # adding right tree if exist
+        if self.right:
+            element += self.right.in_order_traversal()
+
+        # adding root
+        element.append(self.data)
+
+        return element
+
 
 def build_tree(li: list):
-  root = BinaryTree(li[0])
-  for num in li[1:]:
-      root.addChild(num)
-  return root
+    root = BinaryTree(li[0])
+    for num in li[1:]:
+        root.addChild(num)
+    return root
